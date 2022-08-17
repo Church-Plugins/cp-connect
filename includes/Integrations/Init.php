0230b27392382138c76d96136bf67affb21a6134
Tanner Moushey <tanner@missionlab.dev>
2022-08-16 21:50:52 -0500

    init

---
+++ includes/Integrations/Init.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CP_Connect\Integrations;
+
+/**
+ * Setup integration initialization
+ */
+class Init {
+
+	/**
+	 * @var Init
+	 */
+	protected static $_instance;
+
+	/**
+	 * Only make one instance of Init
+	 *
+	 * @return Init
+	 */
+	public static function get_instance() {
+		if ( ! self::$_instance instanceof Init ) {
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+	/**
+	 * Class constructor
+	 *
+	 */
+	protected function __construct() {
+		$this->includes();
+		$this->actions();
+	}
+
+	/**
+	 * Admin init includes
+	 *
+	 * @return void
+	 */
+	protected function includes() {}
+
+	protected function actions() {}
+
+	/** Actions ***************************************************/
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd6abbe30094865ea37e959551eb39a6a806b5ef1',
+    'name' => 'churchplugins/cp-connect',
+  ),
+  'versions' => 
+  array (
+    'churchplugins/cp-connect' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd6abbe30094865ea37e959551eb39a6a806b5ef1',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af93ba6e52236418f07a278033eba6959ee5b983',
+    ),
+    'wpackio/enqueue' => 
+    array (
+      'pretty_version' => '3.4.0',
+      'version' => '3.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8a5604f5ab3bd5f000e503dda02603e15e534d47',
+    ),
+  ),
+);
