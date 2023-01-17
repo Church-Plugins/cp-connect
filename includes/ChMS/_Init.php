cab93834d94e1035eb59662e835334dd0e0ee299
Tanner Moushey <tanner@missionlab.dev>
2023-01-17 15:53:51 -0600

    Remove unused cron

--- includes/ChMS/_Init.php
+++ includes/ChMS/_Init.php
@@ -15,13 +15,6 @@ class _Init {
 	protected static $_instance;
 
 	/**
-	 * The string to use for the cron pull
-	 *
-	 * @var string
-	 */
-	public static $_cron_hook = 'cp_connect_chms_pull';
-
-	/**
 	 * Only make one instance of _Init
 	 *
 	 * @return _Init
@@ -42,6 +35,12 @@ class _Init {
 		$this->actions();
 	}
 
+	protected function actions() {
+		add_action( 'init', [ $this, 'includes' ] );
+	}
+
+	/** Actions ***************************************************/
+	
 	/**
 	 * Admin init includes
 	 *
@@ -58,33 +57,6 @@ class _Init {
 				$pco = PCO::get_instance();
 				break;
 		}
-	}
-
-	protected function actions() {
-		add_action( 'init', [ $this, 'schedule_cron' ] );
-		add_action( 'init', [ $this, 'includes' ] );
-	}
-
-	/** Actions ***************************************************/
-
-	/**
-	 * Schedule the cron to pull data from the ChMS
-	 *
-	 * @since  1.0.0
-	 *
-	 * @author Tanner Moushey
-	 */
-	public function schedule_cron() {
-		if ( wp_next_scheduled( self::$_cron_hook ) ) {
-			return;
-		}
-
-		$args = apply_filters( 'cp_connect_chms_cron_args', [
-			'timestamp' => time(),
-			'recurrence' => 'hourly',
-		] );
-
-		wp_schedule_event( $args[ 'timestamp' ], $args['recurrence'], self::$_cron_hook );
 	}
 
 }
